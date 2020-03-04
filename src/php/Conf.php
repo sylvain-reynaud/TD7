ded87ea0fa9d25918f47b976b4b05c838c69713c
sgchess <sebastien.gagne@umontpellier.fr>
2020-03-04 11:07:21 +0100

    détail

--- src/php/Conf.php
+++ src/php/Conf.php
@@ -4,9 +4,9 @@ class Conf {
 
     private static $database = array(
         'hostname' => 'webinfo.iutmontp.univ-montp2.fr',
-        'database' => 'etugagne',
-        'login'    => 'etugagne',
-        'password' => '31415'
+        'database' => '...', // à compléter avec vos données personnelles
+        'login'    => '...', // à compléter avec vos données personnelles
+        'password' => '...'  // à compléter avec vos données personnelles
     );
 
     static public function getLogin() {
